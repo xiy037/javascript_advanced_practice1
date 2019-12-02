12701d1d86474f4c8503e670aaa3a21c6c099b8b
Xiao Yu <xiao.susan.yu@gmail.com>
2019-12-02 12:12:18 +0800

    [Xiao]{feat}: finish reduce part two

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function spilt_to_zero(number, interval) {
-  //在这里写入代码
+  var result = [number];
+  var element = number;
+  while (element > 0) {
+    element = (element*10 - interval*10)/10;
+    result.push(element);
+  }
+  return result;
 }
 
 module.exports = spilt_to_zero;
